e8ed74f4d044ff4dd0b1ede2c289b8b10de3ec60
willamjonathan <williamjonathan1289@gmail.com>
2023-11-16 14:23:35 +0700

    "alignment color is identified now A = red, etc"

--- five_primate/code/show_alignment.py
+++ five_primate/code/show_alignment.py
@@ -1,6 +1,7 @@
 from Bio import AlignIO
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.colors import ListedColormap
 
 clw_file = "five_primate/code/align_ms5.clw"
 alignment = AlignIO.read(clw_file, "clustal")
@@ -9,6 +10,10 @@ step_size = 200
 
 # Convert the alignment to a numpy array of characters
 alignment_array = np.array([list(rec.seq) for rec in alignment], dtype=np.character)
+
+#  - is for absent
+colors = {'A': 'red', 'C': 'blue', 'G': 'green', 'T': 'orange', '-': 'gray'}
+cmap = ListedColormap([colors[char] for char in "ACGT-"])
 
 # Convert characters to numeric values (ASCII codes)
 alignment_numeric = np.array(
@@ -20,11 +25,11 @@ tick_labels = [str(pos) for pos in tick_positions]
 
 # Plot the alignment with increased figure size, decreased font size, and adjusted tick positions
 fig, ax = plt.subplots(figsize=(15, 10))
-ax.set_title("Multiple Sequence Alignment", fontsize = 18)
+ax.set_title("Multiple Sequence Alignment", fontsize=18)
 ax.set_xticks(tick_positions)
-ax.set_xticklabels(tick_labels, rotation=90, fontsize=8)  # Adjust fontsize as needed
+ax.set_xticklabels(tick_labels, rotation=90, fontsize=8) 
 
-# Draw the alignment
-ax.imshow(alignment_numeric, aspect="auto", cmap="rainbow")
+# Draw the alignment using the custom colormap
+ax.imshow(alignment_numeric, aspect="auto", cmap=cmap)
 
 plt.show()
